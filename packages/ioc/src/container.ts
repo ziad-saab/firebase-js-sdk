a91a03b7476e5b445fa01de8987a8ac9ff3a0fbd
Feiyang1 <plane1113@gmail.com>
2019-02-10 21:40:44 -0800

    ioc implementation

--- packages/ioc/src/container.ts
+++ packages/ioc/src/container.ts
@@ -1,12 +1,138 @@
 
 import { FirebaseApp } from '@firebase/app-types';
+import { Deferred } from '@firebase/util';
 
-interface ServiceFactory {
-  function(app: FirebaseApp): any;
+export interface ServiceFactory<T = any> {
+  (app: FirebaseApp, instanceName?: string): T;
 }
 
+interface GetOptions {
+  instanceName: string;
+}
+
+const DEFAULT_SERVICE_INSTANCE_NAME = '[DEFAULT]';
+
+// service factroy registration
+const registrations: { [name: string]: ServiceFactory } = {};
+
+// all container instances
+const containerInstances = [];
+
 export class Container {
+
+  static get instances(): Container[] {
+    return containerInstances;
+  }
+
+  static get registrations(): { [name: string]: ServiceFactory} {
+    return registrations;
+  }
+
+  private _factories: { [name: string]: ServiceFactory } = {};
+  private _pendingRegistration: { [name: string]: Deferred<any> } = {};
+
+  /**
+   * service instance
+   */
+  private _serviceInstanceCache: {
+    [serviceName: string]: {
+      [instanceName: string]: any
+    }
+  } = {};
+
+  constructor(private _app: FirebaseApp) {
+    containerInstances.push(this);
+
+    // register existing service factories on this container
+    Object.keys(Container.registrations).forEach((key) => {
+      this.register(key, Container.registrations[key]);
+    });
+  }
+
   register(name: string, serviceFactory: ServiceFactory): void {
-    
+    if (typeof serviceFactory !== 'function') {
+      throw Error('invalid-factory');
+    }
+
+    if (this._factories[name]) {
+      throw Error('factory-already-exists');
+    }
+
+    this._factories[name] = serviceFactory;
+
+    // resolve any pending registration
+    if (this._pendingRegistration[name]) {
+      this._pendingRegistration[name].resolve(serviceFactory);
+      delete this._pendingRegistration[name];
+    }
+  }
+
+
+  async get<T = any>(serviceName: string, options: GetOptions): Promise<T> {
+    const instanceName = options.instanceName || DEFAULT_SERVICE_INSTANCE_NAME;
+
+    const cachedInstance = this._getFromCache(serviceName, instanceName);
+    if (cachedInstance) {
+      return cachedInstance;
+    }
+
+    const factory = await this._getFactory(serviceName);
+    const serviceInstance = factory(this._app, instanceName);
+
+    if (!this._serviceInstanceCache[serviceName]) {
+      this._serviceInstanceCache[serviceName] = {};
+    }
+
+    this._serviceInstanceCache[serviceName][instanceName] = serviceInstance;
+    return serviceInstance;
+  }
+
+  getImmediate<T = any>(serviceName: string, options: GetOptions): T {
+    const instanceName = options.instanceName || DEFAULT_SERVICE_INSTANCE_NAME;
+
+    const cachedInstance = this._getFromCache(serviceName, instanceName);
+    if (cachedInstance) {
+      return cachedInstance;
+    }
+
+    if (this._serviceInstanceCache[serviceName] && this._serviceInstanceCache[serviceName][instanceName]) {
+      return this._serviceInstanceCache[serviceName][instanceName];
+    }
+
+    // get factory synchronously without waiting for lazily loaded services
+    const factory = this._factories[serviceName];
+    const serviceInstance = factory(this._app, instanceName);
+
+    if (!this._serviceInstanceCache[serviceName]) {
+      this._serviceInstanceCache[serviceName] = {};
+    }
+
+    this._serviceInstanceCache[serviceName][instanceName] = serviceInstance;
+    return serviceInstance;
+  }
+
+  private _getFromCache(serviceName: string, instanceName: string): any {
+    if (this._serviceInstanceCache[serviceName] && this._serviceInstanceCache[serviceName][instanceName]) {
+      return this._serviceInstanceCache[serviceName][instanceName];
+    }
+  }
+
+  /**
+   * The factory can either be pre-registered or lazily loaded, we handle them
+   * both the same way by getting it asynchronously
+   */
+  private async _getFactory(name: string): Promise<ServiceFactory> {
+    if (this._factories[name]) {
+      return this._factories[name];
+    }
+
+    if (this._pendingRegistration[name]) {
+      return this._pendingRegistration[name].promise;
+    }
+
+    const deferred = new Deferred<ServiceFactory>();
+    this._pendingRegistration[name] = deferred;
+
+    return deferred.promise;
   }
 }
